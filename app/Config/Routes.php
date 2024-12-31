0b7724259391bab19cf1554edf8f70449029aa50
jayblogs <jaydeephax@gmail.com>
2024-12-31 13:57:06 +0530

    added put/patch, get(single) & delete for tasks

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -27,11 +27,11 @@ $routes->get('board', [DashboardController::class, 'dashboard']);
 
 $routes->resource('tasks', ['controller' => '\App\Controllers\API\DeepWorkController']);
 // Equivalent to the following:
-// $routes->get('photos/new', 'Photos::new');
-// $routes->post('photos', 'Photos::create');
+// [NOTA] $routes->get('photos/new', 'Photos::new');
+// [DONE] $routes->post('photos', 'Photos::create');
 // [DONE] $routes->get('photos', 'Photos::index');
-// $routes->get('photos/(:segment)', 'Photos::show/$1');
-// $routes->get('photos/(:segment)/edit', 'Photos::edit/$1');
+// [DONE] $routes->get('photos/(:segment)', 'Photos::show/$1');
+// [FORM] $routes->get('photos/(:segment)/edit', 'Photos::edit/$1');
 // $routes->put('photos/(:segment)', 'Photos::update/$1');
 // $routes->patch('photos/(:segment)', 'Photos::update/$1');
 // $routes->delete('photos/(:segment)', 'Photos::delete/$1');
--- app/Models/DeepWorkModel.php
+++ app/Models/DeepWorkModel.php
@@ -10,7 +10,7 @@ class DeepWorkModel extends Model
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'object';
-    protected $useSoftDeletes   = false;
+    protected $useSoftDeletes   = true;
     protected $protectFields    = true;
     protected $allowedFields    = ['title', 'picture', 'videos', 'description', 'ppm'];
 
